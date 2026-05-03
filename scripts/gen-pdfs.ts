00be7a0516c0294d2524cf8fd2e7ca1ea0cc27c9
Guido Flohr <guido.flohr@cantanea.com>
2026-05-03 20:01:46 +0300

    produce type1-fonts-embedded.pdf with LibreOffice

--- scripts/gen-pdfs.ts
+++ scripts/gen-pdfs.ts
@@ -88,52 +88,9 @@ async function genType1FontsMissing(): Promise<void> {
 	console.log(`written ${filename}`);
 }
 
-async function genType1FontsEmbedded(): Promise<void> {
-	const draw = async (
-		pdfDoc: PDFDocument,
-		label: string,
-		font: StandardFonts | Uint8Array<ArrayBufferLike>,
-	): Promise<void> => {
-		const page = pdfDoc.addPage();
-
-		const f = await pdfDoc.embedFont(font);
-
-		page.drawText(label, {
-			x: 50,
-			y: page.getSize().height - 50,
-			size: 14,
-			font: f,
-			color: rgb(0, 0, 0),
-		});
-	};
-
-	const pdfDoc = await PDFDocument.create();
-	pdfDoc.registerFontkit(fontkit);
-
-	const sansBytes = await fs.readFile('./assets/fonts/noto/NotoSans-Regular.ttf');
-	await draw(pdfDoc, 'This page uses Helvetica.', sansBytes);
-
-	const serifBytes = await fs.readFile('./assets/fonts/noto/NotoSans-Regular.ttf');
-	await draw(
-		pdfDoc,
-		'ÄÖÜäöüß in Times-Roman.',
-		serifBytes,
-	);
-	const symbolBytes = await fs.readFile('./assets/fonts/noto/NotoSansSymbols-Regular.ttf');
-	await draw(pdfDoc, 'ΑαΒβΓγΔδ', symbolBytes);
-	// FIXME! This produces garbage.
-	await draw(pdfDoc, '✂✈✉☎✔✘★', symbolBytes);
-
-	const bytes = await pdfDoc.save();
-	const filename = './assets/pdfs/type1-fonts-embedded.pdf';
-	await fs.writeFile(filename, bytes);
-	console.log(`written ${filename}`);
-}
-
 async function genAll() {
 	await genStandardFonts();
 	await genType1FontsMissing();
-	await genType1FontsEmbedded();
 }
 
 genAll().catch((e) => {
