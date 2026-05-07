b818440e3fd36b04d6df9eaf70b39f8b3e32844d
Guido Flohr <guido.flohr@cantanea.com>
2026-05-07 20:54:45 +0300

    write PDF with fake ToUnicode CMap

--- scripts/gen-pdfs.ts
+++ scripts/gen-pdfs.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
-import { PDFDocument, rgb, StandardFonts } from '@cantoo/pdf-lib';
+import { PDFArray, PDFDict, PDFDocument, PDFName, PDFPage, PDFRawStream, PDFRef, PDFStream, rgb, StandardFonts } from '@cantoo/pdf-lib';
 import fontkit from '@pdf-lib/fontkit';
 
 async function genStandardFonts(): Promise<void> {
@@ -84,9 +84,113 @@ async function genType1FontsMissing(): Promise<void> {
 	console.log(`written ${filename}`);
 }
 
+const encodingTest = `q
+BT
+0 0 0 rg
+/{helvetica} 12 Tf
+24 TL
+1 0 0 1 50 791.89 Tm
+% When this text gets copied and pasted, it will "magically" change.
+(price: $5) Tj
+T*
+ET
+Q
+`;
+
+const fakeCMap = `CIDInit /ProcSet findresource begin
+12 dict begin
+begincmap
+/CIDSystemInfo <<
+  /Registry (Adobe)
+  /Ordering (UCS)
+  /Supplement 0
+>> def
+/CMapName /Adobe-Identity-UCS def
+/CMapType 2 def
+1 begincodespacerange
+<00><ff>
+endcodespacerange
+% Fake CMAp that turns "price: $5" into "Preis: [Euro sign] 2".
+8 beginbfchar
+<70> <0050> % p -> P
+<72> <0072> % r -> r
+<69> <0065> % i -> e
+<63> <0069> % c -> i
+<65> <0073> % e -> s
+<20> <0020> % space
+<24> <20ac> % $ -> Euro sign
+<35> <0032> % 5 -> 2
+endbfchar
+endcmap
+CMapName currentdict /CMap defineresource pop
+end
+`;
+async function genEncodingTest() {
+	const pdfDoc = await PDFDocument.create();
+	const context = pdfDoc.context;
+	const page = pdfDoc.addPage();
+
+	const helvetica = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+	await pdfDoc.save();
+
+	const y = page.getSize().height - 50;
+	page.drawText('Test test', {
+		x: 50, y, size: 12, font: helvetica, color: rgb(0, 0, 0),
+	});
+	const helveticaName = getFontNameByRef(page, helvetica.ref).decodeText();
+	const newContent = encodingTest.replace('{helvetica}', helveticaName);
+
+	const contentStream = getContentStream(page);
+	contentStream.updateContents(new TextEncoder().encode(newContent));
+	contentStream.dict.delete(PDFName.of('Filter'));
+
+	const cmapStream = context.flateStream(fakeCMap);
+	const cmapRef = context.register(cmapStream);
+	const helveticaDict = context.lookupMaybe(helvetica.ref, PDFDict)!;
+	helveticaDict.set(PDFName.of('ToUnicode'), cmapRef);
+
+	const bytes = await pdfDoc.save();
+	const filename = './assets/pdfs/encoding-test.pdf';
+	await fs.writeFile(filename, bytes);
+	console.log(`written ${filename}`);
+}
+
+function getFontNameByRef(page: PDFPage, ref: PDFRef) {
+	const { Font } = page.node.normalizedEntries();
+	const refString = ref.toString();
+	const entries = Font.entries();
+
+	for (let i = 0; i < entries.length; ++i) {
+		const [fontName, fontRef] = entries[i];
+		if (fontRef.toString() === refString) {
+			return fontName;
+		}
+	}
+
+	throw new Error(`no font resource '${ref}'`)
+}
+
+function getContentStream(page: PDFPage): PDFStream {
+	const { Contents } = page.node.normalizedEntries();
+	if (!(Contents instanceof PDFArray)) throw new Error('no contents array');
+
+	for (let j = 0; j < Contents.size(); ++j) {
+		const item = Contents.get(j);
+		const resolved = page.doc.context.lookup(item);
+
+		if (resolved instanceof PDFStream) {
+			return resolved;
+		}
+	}
+
+	throw new Error('no contents stream');
+}
+
 async function genAll() {
 	await genStandardFonts();
 	await genType1FontsMissing();
+	await genEncodingTest();
 }
 
 genAll().catch((e) => {
