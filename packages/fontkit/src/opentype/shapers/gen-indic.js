0890d55e5e897e54121e8077f3808396b184db3a
Guido Flohr <guido.flohr@cantanea.com>
2026-05-11 20:16:12 +0300

    fix linter issues

--- packages/fontkit/src/opentype/shapers/gen-indic.js
+++ packages/fontkit/src/opentype/shapers/gen-indic.js
@@ -247,11 +247,12 @@ for (let i = 0; i < codepoints.length; i++) {
 
 // Trie is serialized suboptimally as JSON so it can be loaded via require,
 // allowing unicode-properties to work in the browser
+// biome-ignore lint/style/useTemplate: breaks things
 const trieFilePath = __dirname + '/trieIndic.json';
 const jsonBase64DeflatedTrie = JSON.stringify(
 	base64.encode(pako.deflate(trie.toBuffer())),
 );
-fs.writeFileSync(trieFilePath, jsonBase64DeflatedTrie);
+fs.writeFileSync(trieFilePath, `${jsonBase64DeflatedTrie}\n`);
 
 const stateMachine = compile(
 	fs.readFileSync(__dirname + '/indic.machine', 'utf8'),
