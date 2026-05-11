f2c1f857b57a50821be7db1397a9a6818eb91af9
Guido Flohr <guido.flohr@cantanea.com>
2026-05-11 16:12:22 +0300

    fix linter issues in TrueTypeCollection

--- packages/fontkit/src/TrueTypeCollection.js
+++ packages/fontkit/src/TrueTypeCollection.js
@@ -1,57 +1,57 @@
 import r from '@pdf-lib/restructure';
 import TTFFont from './TTFFont';
-import Directory from './tables/directory';
 import tables from './tables';
+import Directory from './tables/directory';
 
-let TTCHeader = new r.VersionedStruct(r.uint32, {
-  0x00010000: {
-    numFonts:   r.uint32,
-    offsets:    new r.Array(r.uint32, 'numFonts')
-  },
-  0x00020000: {
-    numFonts:   r.uint32,
-    offsets:    new r.Array(r.uint32, 'numFonts'),
-    dsigTag:    r.uint32,
-    dsigLength: r.uint32,
-    dsigOffset: r.uint32
-  }
+const TTCHeader = new r.VersionedStruct(r.uint32, {
+	65536: {
+		numFonts: r.uint32,
+		offsets: new r.Array(r.uint32, 'numFonts'),
+	},
+	131072: {
+		numFonts: r.uint32,
+		offsets: new r.Array(r.uint32, 'numFonts'),
+		dsigTag: r.uint32,
+		dsigLength: r.uint32,
+		dsigOffset: r.uint32,
+	},
 });
 
 export default class TrueTypeCollection {
-  static probe(buffer) {
-    return buffer.toString('ascii', 0, 4) === 'ttcf';
-  }
+	static probe(buffer) {
+		return buffer.toString('ascii', 0, 4) === 'ttcf';
+	}
 
-  constructor(stream) {
-    this.stream = stream;
-    if (stream.readString(4) !== 'ttcf') {
-      throw new Error('Not a TrueType collection');
-    }
+	constructor(stream) {
+		this.stream = stream;
+		if (stream.readString(4) !== 'ttcf') {
+			throw new Error('Not a TrueType collection');
+		}
 
-    this.header = TTCHeader.decode(stream);
-  }
+		this.header = TTCHeader.decode(stream);
+	}
 
-  getFont(name) {
-    for (let offset of this.header.offsets) {
-      let stream = new r.DecodeStream(this.stream.buffer);
-      stream.pos = offset;
-      let font = new TTFFont(stream);
-      if (font.postscriptName === name) {
-        return font;
-      }
-    }
+	getFont(name) {
+		for (const offset of this.header.offsets) {
+			const stream = new r.DecodeStream(this.stream.buffer);
+			stream.pos = offset;
+			const font = new TTFFont(stream);
+			if (font.postscriptName === name) {
+				return font;
+			}
+		}
 
-    return null;
-  }
+		return null;
+	}
 
-  get fonts() {
-    let fonts = [];
-    for (let offset of this.header.offsets) {
-      let stream = new r.DecodeStream(this.stream.buffer);
-      stream.pos = offset;
-      fonts.push(new TTFFont(stream));
-    }
+	get fonts() {
+		const fonts = [];
+		for (const offset of this.header.offsets) {
+			const stream = new r.DecodeStream(this.stream.buffer);
+			stream.pos = offset;
+			fonts.push(new TTFFont(stream));
+		}
 
-    return fonts;
-  }
+		return fonts;
+	}
 }
