7974cae398a744b4e15d14886e7a048a08c85201
Guido Flohr <guido.flohr@cantanea.com>
2026-05-14 15:05:12 +0300

    document xMaxExtent

--- packages/fontkit/src/types/internal/tables/hhea.ts
+++ packages/fontkit/src/types/internal/tables/hhea.ts
@@ -9,6 +9,11 @@ export interface HheaTable {
 	advanceWidthMax: number /** The maximum advance width value in 'hmtx' table. */;
 	minLeftSideBearing: number /** /** The minimum left sidebearing value. */
 	minRightSideBearing: number /** The minimum right sidebearing value. */;
+
+	/**
+	 * The maximum horizontal extent of any glyph in the font.
+	 * Used for fast text layout and bounding box estimation.
+	 */
 	xMaxExtent: number;
 	caretSlopeRise: number /** Used to calculate the slope of the cursor (rise/run) 1 for vertical. */;
 	caretSlopeRun: number /** 0 for vertical. */;
