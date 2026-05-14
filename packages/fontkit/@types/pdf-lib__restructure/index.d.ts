ed55717d531fb492749fd58d768aa33ae12d6530
Guido Flohr <guido.flohr@cantanea.com>
2026-05-14 21:19:56 +0300

    migrate cmap, maxp, and hmtx tables

--- packages/fontkit/@types/pdf-lib__restructure/index.d.ts
+++ packages/fontkit/@types/pdf-lib__restructure/index.d.ts
@@ -1,4 +1,9 @@
 /** biome-ignore-all lint/suspicious/noExplicitAny: this file models a highly dynamic runtime parsing DSL. */
+
+// This is an attempt to provide types for `@pdf-lib/restructure` the way
+// the library is used inside of this project. It is very well possible that
+// legitimate usage of `restructure` is not reflected here. Do not suspect
+// a bug in this case but rather extend the typing.
 declare module '@pdf-lib/restructure' {
 	type InferStruct<TFields> = {
 		[K in keyof TFields]: TFields[K] extends Field<infer TValue>
@@ -22,7 +27,7 @@ declare module '@pdf-lib/restructure' {
 	export type LengthResolver = (context: any) => number;
 
 	type StructFields = {
-		[key: string]: Field<any> | StructFields;
+		[key: string]: Field<any> | StructFields | ((context: any) => any);
 	};
 	export class Struct<TFields extends StructFields>
 		implements Field<InferStruct<TFields>>
@@ -43,7 +48,13 @@ declare module '@pdf-lib/restructure' {
 	export class FieldArray<TField extends Field<any>>
 		implements Field<TField extends Field<infer T> ? T[] : never>
 	{
-		constructor(field: TField, length?: number | string | LengthResolver);
+		constructor(field: TField, length?: number | string | LengthResolver | Field<number>);
+	}
+
+	export class LazyArray<TField extends Field<any>>
+		implements Field<TField extends Field<infer T> ? T[] : never>
+	{
+		constructor(field: TField, length?: number | string | LengthResolver | Field<number>);
 	}
 
 	export class Bitfield<const T extends readonly (string | null)[]>
@@ -53,11 +64,24 @@ declare module '@pdf-lib/restructure' {
 	}
 
 	export class Reserved {
-		constructor(type: unknown, count: number);
+		constructor(type: unknown, count?: number);
 	}
 
 	export class RString implements Field<string> {
 		constructor(length: number | Field<number>);
+	}
+
+	export class Pointer<TField extends Field<any>>
+		implements Field<TField extends Field<infer T> ? T : never>
+	{
+		constructor(
+			offset: Field<number>,
+			target: TField,
+			options?: {
+				type?: 'parent' | 'root';
+				lazy?: boolean;
+			}
+		);
 	}
 
 	export interface RestructureStatic {
@@ -65,13 +89,17 @@ declare module '@pdf-lib/restructure' {
 		VersionedStruct: typeof VersionedStruct;
 		Reserved: typeof Reserved;
 		Array: typeof FieldArray;
+		LazyArray: typeof LazyArray;
 		Bitfield: typeof Bitfield;
 		String: typeof RString;
+		Pointer: typeof Pointer;
 
 		int8: Field<number>;
 		uint8: Field<number>;
 		int16: Field<number>;
 		uint16: Field<number>;
+		int24: Field<number>;
+		uint24: Field<number>;
 		int32: Field<number>;
 		uint32: Field<number>;
 		float: Field<number>;
