68824c4ad586ea07de7512d6c538b9a8ba807245
Guido Flohr <guido.flohr@cantanea.com>
2026-05-15 13:23:40 +0300

    type restructure, wip

--- packages/fontkit/@types/pdf-lib__restructure/index.d.ts
+++ packages/fontkit/@types/pdf-lib__restructure/index.d.ts
@@ -5,16 +5,6 @@
 // legitimate usage of `restructure` is not reflected here. Do not suspect
 // a bug in this case but rather extend the typing.
 declare module '@pdf-lib/restructure' {
-	type InferStruct<TFields> = {
-		[K in keyof TFields]: TFields[K] extends Field<infer TValue>
-			? TValue
-			: never;
-	};
-
-	export interface Field<T = unknown> {
-		readonly __type?: T;
-	}
-
 	export class DecodeStream {
 		buffer: Uint8Array;
 		pos: number;
@@ -24,96 +14,228 @@ declare module '@pdf-lib/restructure' {
 		readUInt32BE(): number;
 	}
 
-	export type LengthResolver = (context: any) => number;
+	export interface FieldT<T> {
+		readonly __type?: T;
 
-	export type PathRef = string;
+		size(): number;
 
-	type StructFields = {
-		[key: string]: Field<any> | StructFields | ((context: any) => any);
+		decode(stream: DecodeStream): T;
+
+		encode(stream: DecodeStream, val: T): void;
+	}
+
+	export type LengthResolver = (parent?: FieldT<unknown>) => number;
+
+	export type Length = number | string | LengthResolver;
+
+	export class NumberT implements FieldT<number> {
+		readonly __type?: number;
+
+		constructor(type: string, endian?: string);
+
+		size(): number;
+
+		decode(stream: DecodeStream): number;
+
+		encode(stream: DecodeStream, val: number): void;
+	}
+
+	export class StringT implements FieldT<string> {
+		readonly __type?: string;
+
+		constructor(length?: Length, encoding?: string);
+
+		size(value?: string, parent?: FieldT<unknown>): number;
+
+		decode(stream: DecodeStream, parent?: FieldT<unknown>): string;
+
+		encode(stream: DecodeStream, value: string, parent?: FieldT<unknown>): void;
+	}
+
+	type InferField<T> = T extends FieldT<infer TValue> ? TValue : never;
+
+	export class ArrayT<TField extends FieldT<unknown>>
+		implements FieldT<InferField<TField>[]>
+	{
+		readonly __type?: InferField<TField>[];
+
+		constructor(type: TField, length?: Length, lengthType?: 'count' | 'bytes');
+
+		size(items?: FieldT<TField>[], parent?: FieldT<unknown>): number;
+
+		decode(stream: DecodeStream): InferField<TField>[];
+
+		encode(stream: DecodeStream, value: InferField<TField>[]): void;
+	}
+
+	export class LazyArrayT<TField extends FieldT<unknown>>
+		implements FieldT<InferField<TField>[]>
+	{
+		readonly __type?: InferField<TField>[];
+
+		constructor(type: TField, length?: Length, lengthType?: 'count' | 'bytes');
+
+		size(items?: FieldT<TField>[], parent?: FieldT<unknown>): number;
+
+		decode(stream: DecodeStream): InferField<TField>[];
+
+		encode(stream: DecodeStream, value: InferField<TField>[]): void;
+	}
+
+	type StructFields = Record<string, FieldT<any>>;
+
+	type InferStruct<TFields extends StructFields> = {
+		[K in keyof TFields]: InferField<TFields[K]>;
 	};
 
-	export class Struct<TFields extends StructFields>
-		implements Field<InferStruct<TFields>>
+	export class StructT<TFields extends StructFields>
+		implements FieldT<InferStruct<TFields>>
 	{
+		readonly __type?: InferStruct<TFields>;
+
 		constructor(fields: TFields);
+
+		size(value?: InferStruct<TFields>, parent?: FieldT<unknown>): number;
+
+		decode(
+			stream: DecodeStream,
+			parent?: FieldT<unknown>,
+			length?: number,
+		): InferStruct<TFields>;
+
+		encode(
+			stream: DecodeStream,
+			value: InferStruct<TFields>,
+			parent?: FieldT<unknown>,
+		): void;
 	}
 
-	export class VersionedStruct<TFields extends StructFields>
-		implements Field<InferStruct<TFields>>
-	{
-		readonly __type: InferStruct<TFields>;
+	export class VersionedStructT<
+		TVersions extends Record<string, StructFields>
+	> implements FieldT<
+		{
+			[K in keyof TVersions & string]:
+				{ version: K } & InferStruct<TVersions[K]>
+		}[keyof TVersions & string]
+	> {
+		readonly __type?: {
+			[K in keyof TVersions & string]:
+				{ version: K } & InferStruct<TVersions[K]>
+		}[keyof TVersions & string];
 
-		constructor(version: Field<number> | PathRef, fields: TFields);
+		constructor(
+			type: string | FieldT<number>,
+			versions: TVersions
+		);
 
-		versions: Record<number, TFields>;
+		decode(
+			stream: DecodeStream,
+			parent?: any,
+			length?: number
+		): {
+			[K in keyof TVersions & string]:
+				{ version: K } & InferStruct<TVersions[K]>
+		}[keyof TVersions & string];
 
-		[key: string]: any; // Make it augmentable.
+		size(value?: any, parent?: any): number;
+
+		encode(stream: DecodeStream, value: this['__type'], parent?: unknown): void;
 	}
 
-	export class FieldArray<TField extends Field<any>>
-		implements Field<TField extends Field<infer T> ? T[] : never>
-	{
-		constructor(field: TField, length?: number | string | LengthResolver | Field<number>);
-	}
-
-	export class LazyArray<TField extends Field<any>>
-		implements Field<TField extends Field<infer T> ? T[] : never>
-	{
-		constructor(field: TField, length?: number | string | LengthResolver | Field<number>);
-	}
-
-	export type BitfieldResult<T extends readonly (string | null)[]> = {
+	type BitfieldResult<T extends readonly (string | null)[]> = {
 		[K in Exclude<T[number], null>]: boolean;
 	};
 
-	export class Bitfield<const T extends readonly (string | null)[]>
-		implements Field<BitfieldResult<T>>
+	export class BitfieldT<TFlags extends readonly (string | null)[]>
+		implements FieldT<BitfieldResult<TFlags>>
 	{
-		constructor(field: Field<number>, names: T);
+		constructor(type: FieldT<number>, flags: TFlags);
+
+		decode(stream: DecodeStream): BitfieldResult<TFlags>;
+
+		size(): number;
+
+		encode(stream: DecodeStream, value: BitfieldResult<TFlags>): void;
 	}
 
-	export class Reserved {
-		constructor(type: unknown, count?: number);
-	}
-
-	export class RString implements Field<string> {
-		constructor(length: number | Field<number>);
-	}
-
-	export class Pointer<TField extends Field<any>>
-		implements Field<TField extends Field<infer T> ? T : never>
-	{
+	export class PointerT<
+		TField extends FieldT<any>
+	> implements FieldT<
+		| InferField<TField>
+		| number
+		| null
+		| { get: () => InferField<TField> }
+	> {
 		constructor(
-			offset: Field<number>,
-			target: TField,
+			offsetType: FieldT<number>,
+			type: TField | 'void',
 			options?: {
-				type?: 'parent' | 'root';
+				type?: 'local' | 'immediate' | 'parent' | 'global';
+				allowNull?: boolean;
+				nullValue?: number;
 				lazy?: boolean;
+				relativeTo?: string;
 			}
 		);
+
+		decode(
+			stream: DecodeStream,
+			ctx?: unknown
+		):
+			| InferField<TField>
+			| number
+			| null
+			| { get: () => InferField<TField> };
+
+		size(value?: unknown, ctx?: unknown): number;
+
+		encode(stream: DecodeStream, value: unknown, ctx?: unknown): void;
 	}
 
-	export interface RestructureStatic {
-		Struct: typeof Struct;
-		VersionedStruct: typeof VersionedStruct;
-		Reserved: typeof Reserved;
-		Array: typeof FieldArray;
-		LazyArray: typeof LazyArray;
-		Bitfield: typeof Bitfield;
-		String: typeof RString;
-		Pointer: typeof Pointer;
+	export class VoidPointer<T = any> {
+		constructor(
+			type: FieldT<T>,
+			value: T
+		);
 
-		int8: Field<number>;
-		uint8: Field<number>;
-		int16: Field<number>;
-		uint16: Field<number>;
-		int24: Field<number>;
-		uint24: Field<number>;
-		int32: Field<number>;
-		uint32: Field<number>;
-		float: Field<number>;
-		double: Field<number>;
-		fixed32: Field<number>;
+		type: FieldT<T>;
+		value: T;
+	}
+
+	export class ReservedT implements FieldT<void> {
+		constructor(
+			type: FieldT<any>,
+			count?: number | string | LengthResolver
+		);
+
+		decode(stream: DecodeStream, parent?: any): undefined;
+
+		size(value?: undefined, parent?: any): number;
+
+		encode(stream: DecodeStream, value: undefined, parent?: any): void;
+	}
+	export interface RestructureStatic {
+		Number: typeof NumberT;
+		String: typeof StringT;
+		Array: typeof ArrayT;
+		LazyArray: typeof LazyArrayT;
+		Struct: typeof StructT;
+		VersionedStruct: typeof VersionedStructT;
+		Bitfield: typeof BitfieldT;
+		Pointer: typeof PointerT;
+		Reserved: typeof ReservedT;
+
+		int8: NumberT;
+		uint8: NumberT;
+		int16: NumberT;
+		uint16: NumberT;
+		int24: NumberT;
+		uint24: NumberT;
+		int32: NumberT;
+		uint32: NumberT;
+		float: NumberT;
+		double: NumberT;
+		fixed32: NumberT;
 	}
 
 	const r: RestructureStatic;
