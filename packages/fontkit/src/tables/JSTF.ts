9c882673ebeb1f6cefb1f190cfa5026e6d55cada
Guido Flohr <guido.flohr@cantanea.com>
2026-05-18 11:27:10 +0300

    migrate JSTF test to TypeScript

--- packages/fontkit/src/tables/JSTF.ts
+++ packages/fontkit/src/tables/JSTF.ts
@@ -9,12 +9,12 @@ const JstfPriority = new r.Struct({
 	shrinkageDisableGSUB: new r.Pointer(r.uint16, JstfGSUBModList),
 	shrinkageEnableGPOS: new r.Pointer(r.uint16, JstfGSUBModList),
 	shrinkageDisableGPOS: new r.Pointer(r.uint16, JstfGSUBModList),
-	shrinkageJstfMax: new r.Pointer(r.uint16, new LookupList(GPOSLookup)),
+	shrinkageJstfMax: new r.Pointer(r.uint16, LookupList(GPOSLookup)),
 	extensionEnableGSUB: new r.Pointer(r.uint16, JstfGSUBModList),
 	extensionDisableGSUB: new r.Pointer(r.uint16, JstfGSUBModList),
 	extensionEnableGPOS: new r.Pointer(r.uint16, JstfGSUBModList),
 	extensionDisableGPOS: new r.Pointer(r.uint16, JstfGSUBModList),
-	extensionJstfMax: new r.Pointer(r.uint16, new LookupList(GPOSLookup)),
+	extensionJstfMax: new r.Pointer(r.uint16, LookupList(GPOSLookup)),
 });
 
 const JstfLangSys = new r.Array(
