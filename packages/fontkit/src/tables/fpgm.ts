838d72228d99d3dd20c68e351f032adf7f5e178c
Guido Flohr <guido.flohr@cantanea.com>
2026-06-06 18:59:49 +0300

    fix fpgm table

--- packages/fontkit/src/tables/fpgm.ts
+++ packages/fontkit/src/tables/fpgm.ts
@@ -1,8 +1,20 @@
 import r from '@pdf-lib/restructure';
 
-// A list of instructions that are executed once when a font is first used.
-// These instructions are known as the font program. The main use of this table
-// is for the definition of functions that are used in many different glyph programs.
-export default new r.Struct({
+export namespace fpgmTable {
+	/**
+	 * A list of instructions that are executed once when a font is first used.
+	 * These instructions are known as the font program. The main use of this
+	 * table is for the definition of functions that are used in many different
+	 * glyph programs.
+	 */
+	export interface fpgm {
+		instructions: number[];
+	}
+}
+
+const fpgmStructFields = {
 	instructions: new r.Array(r.uint8),
-});
+};
+export default new r.Struct<typeof fpgmStructFields, fpgmTable.fpgm>(
+	fpgmStructFields,
+);
