72e38557955994531a8f295b05d297dab43e0dc7
Guido Flohr <guido.flohr@cantanea.com>
2026-06-24 17:31:37 +0300

    migrate index to TypeScript

--- packages/fontkit/src/base.ts
+++ packages/fontkit/src/base.ts
@@ -1,15 +1,22 @@
 import r from '@pdf-lib/restructure';
+import type DFont from './DFont';
+import type { SFNTFont } from './sfnt-font';
+import type TrueTypeCollection from './true-type-collection';
 
-const formats = [];
+export type FontFormatClass = (new (...args: any[]) => SFNTFont | TrueTypeCollection | DFont) & {
+	probe(buffer: Buffer): boolean;
+};
+
+const formats: FontFormatClass[] = [];
 
 const fontkit = {
 	logErrors: false,
 
-	registerFormat: (format) => {
+	registerFormat: (format: FontFormatClass) => {
 		formats.push(format);
 	},
 
-	create: (uint8ArrayFontData, postscriptName) => {
+	create: (uint8ArrayFontData: Uint8Array, postscriptName?: string) => {
 		const buffer = Buffer.from(uint8ArrayFontData);
 		for (let i = 0; i < formats.length; i++) {
 			const format = formats[i];
