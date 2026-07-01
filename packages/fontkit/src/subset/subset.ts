dcbfbddf72a082d2f64077450f99bfffa2ef6273
Guido Flohr <guido.flohr@cantanea.com>
2026-07-01 16:44:34 +0300

    add import extensions

--- packages/fontkit/src/subset/subset.ts
+++ packages/fontkit/src/subset/subset.ts
@@ -1,6 +1,6 @@
 import r, { type EncodeStream } from '@pdf-lib/restructure';
-import type { Glyph } from '../glyph/glyph';
-import type { SFNTFont } from '../sfnt-font';
+import type { Glyph } from '../glyph/glyph.js';
+import type { SFNTFont } from '../sfnt-font.js';
 
 export abstract class Subset {
 	protected readonly glyphs: number[];
