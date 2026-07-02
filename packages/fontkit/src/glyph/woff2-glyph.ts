953b6f03c8fe712514960dc74b59c81094519f5d
Guido Flohr <guido.flohr@cantanea.com>
2026-07-02 19:25:49 +0300

    guard against missing tables

--- packages/fontkit/src/glyph/woff2-glyph.ts
+++ packages/fontkit/src/glyph/woff2-glyph.ts
@@ -1,4 +1,4 @@
-import { TrueTypeSubsetFont } from '../true-type-subset-font.js';
+import { requiredTrueTypeSubsetTables, TrueTypeSubsetFont } from '../true-type-subset-font.js';
 import type { WOFF2Font } from '../woff2-font.js';
 import { type DecodedGlyph, TTFGlyph } from './ttf-glyph.js';
 
@@ -6,8 +6,17 @@ import { type DecodedGlyph, TTFGlyph } from './ttf-glyph.js';
  * Represents a TrueType glyph in the WOFF2 format, which compresses glyphs differently.
  */
 export class WOFF2Glyph extends TTFGlyph {
-	constructor(id: number, codePoints: readonly number[], font: WOFF2Font) {
-		super(id, codePoints, font.asTrueTypeSubsetFont());
+	constructor(id: number, codePoints: readonly number[], _font: WOFF2Font) {
+		const font = _font.asTrueTypeSubsetFont();
+		if (!font) {
+			throw new Error(
+				`Fontkit Initialization Error: Cannot construct WOFF2Glyph ` +
+				`for ID ${id}. The underlying WOFF2Font is missing required ` +
+				`TrueType subset tables ` +
+				`(${requiredTrueTypeSubsetTables.join(', ')}).`
+			);
+		}
+		super(id, codePoints, font);
 	}
 
 	decode() {
