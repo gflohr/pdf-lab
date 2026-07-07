c6a211577e78ae65b9e99a581ce8c5901aec3765
Guido Flohr <guido.flohr@cantanea.com>
2026-07-07 18:26:15 +0300

    fix copy & paste error

--- packages/fontkit/src/tables/GPOS.ts
+++ packages/fontkit/src/tables/GPOS.ts
@@ -61,7 +61,9 @@ export class ValueRecord implements r.FieldT<GPOSTable.DecodedValueRecord> {
 	 * Walks up the parent chain to find the structural configuration
 	 * and dynamically constructs the appropriate layout.
 	 */
-	private buildStruct(parent: any): r.StructT<GPOSTable.DecodedValueRecord, any> {
+	private buildStruct(
+		parent: any,
+	): r.StructT<GPOSTable.DecodedValueRecord, any> {
 		let struct = parent;
 
 		// Crawl up the hierarchy until we find the format dictionary and a parent
