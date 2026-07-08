34148eac4c59eaae4972db7be91b798323f87da8
Guido Flohr <guido.flohr@cantanea.com>
2026-07-08 07:57:21 +0300

    copy upstream regression test
    It looks like the issue is already fixed in the @pdf-lab/fontkit codebase.

--- packages/fontkit/test/upstream-bugfixes.test.ts
+++ packages/fontkit/test/upstream-bugfixes.test.ts
@@ -2,7 +2,6 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import { describe, expect, it } from 'vitest';
 import { TrueTypeFont } from '../src';
-import { readSubsetStream } from './helpers.js';
 
 describe('bugfixes from foliojs/fontkit', () => {
 	describe('CFF Subset Binary Layout', () => {
@@ -30,6 +29,25 @@ describe('bugfixes from foliojs/fontkit', () => {
 
 			// The 4th byte (index 3) is the offSize field in a CFF block.
 			expect(buf[3]).toBe(4);
+		});
+	});
+
+	describe('upstream #282 - ReferenceError: Cannot access \'c3x\' before initialization', () => {
+        it('should not throw a ReferenceError', async () => {
+			const fontPath = path.join(
+				__dirname,
+				'data',
+				'PlayfairDisplay',
+				'PlayfairDisplay-Regular.otf',
+			);
+			const fontBytes = await fs.readFile(fontPath);
+			const font = new TrueTypeFont(fontBytes);
+
+			const glyph = font.getGlyph(5);
+			expect(glyph).not.toBeNull();
+
+			// FIXME! Do not rely on getter side-effect.
+			expect(() => glyph!.path).not.toThrow();
 		});
 	});
 });
