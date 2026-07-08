99d7c968059ebe7a2b6f3e2e4fa2fd3fa1099123
Guido Flohr <guido.flohr@cantanea.com>
2026-07-08 12:10:04 +0300

    use optional chaining

--- packages/fontkit/test/open-type.test.ts
+++ packages/fontkit/test/open-type.test.ts
@@ -9,11 +9,11 @@ describe('opentype', async () => {
 	const font = new TrueTypeFont(fontBytes);
 
 	it('featureParams nameID of stylistic set should be 257', () => {
-		expect(font.GSUB?.featureList[150]?.feature.featureParams.nameID).toBe(257);
+		expect(font.GSUB?.featureList[150]?.feature.featureParams?.nameID).toBe(257);
 	});
 
 	it('featureParams version of stylistic set should be 0', () => {
-		expect(font.GSUB?.featureList[150]?.feature.featureParams.version).toBe(0);
+		expect(font.GSUB?.featureList[150]?.feature.featureParams?.version).toBe(0);
 	});
 
 	it('featureParams should be null of aalt opentype feature', () => {
